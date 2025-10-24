f1fafc68a7d68d296c3a59798f8bf4622e09ed7e
Anmol <ag449585@gmail.com>
2025-10-24 18:15:13 +0530

    Initial commit

--- src/main/java/com/wholesaleconnect/backend/controller/TestController.java
+++ src/main/java/com/wholesaleconnect/backend/controller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping
     public String test() {
-        return "WholesaleConnect Backend is Working! 🚀";
+        return "WholesaleConnect Backend is Working 🚀";
     }
 
     @GetMapping("/hello/{name}")
